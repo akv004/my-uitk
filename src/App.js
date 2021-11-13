b032ea504b0867f9aa189b12af184cf47f3969a8
Amit Verma <akv004@gmail.com>
2021-11-13 11:37:32 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
 import Card from "./Card";
 
+const theme = {
+  colorBackgroundLayout: "#E7E9F1",
+  dropShadowCard: "1px 0 12px 0 rgba(0, 0, 0, 0.5)"
+};
 export default function App() {
   return (
     <div className="App">
-      <Card header={`abc`} />
+      <Card header={`abc`} theme={theme} />
     </div>
   );
 }
